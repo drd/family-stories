943f739cf4c217df972d041f27243bc114e20f8a
Eric O'Connell <eric.oconnell@idealist.org>
2015-12-15 23:08:51 -0800

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+    render() {
+        return <div>
+            <h1>Your family. Your stories.</h1>
+            <p>Get started entering your family tree!</p>
+        </div>
+    }
+};
